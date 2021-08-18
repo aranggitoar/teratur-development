03c7e85d3615775eeb96718274f7dd1eb0394709
Aranggi Toar <aranggi.josef@gmail.com>
2021-08-18 13:46:10 +0700

    Added an npm script and changed authorship in footer.

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'teratur' ), 'teratur', '<a href="https://automattic.com/">Automattic</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'teratur' ), 'teratur', '<a href="https://aranggitoar.com/">Aranggi Toar</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->

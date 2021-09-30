24ec22b6a32641cc5485df749f71b8f7ee936d79
aranggitoar <aranggi.josef@gmail.com>
2021-09-30 17:25:20 +0700

    Styled the front page for the portrait alternative and tried styling the header image

--- template-parts/tutor-index-main-info.php
+++ template-parts/tutor-index-main-info.php
@@ -13,7 +13,11 @@
 ?>
 
 <section id="tutor-index-main-info-container">
-<?php get_header_image(); ?>
+<?php if ( get_header_image() ) : ?>
+	<div id="site-header">
+		<img src="<?php header_image(); ?>" width="<?php echo absint( get_custom_header()->width ); ?>" height="<?php echo absint( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+	</div>
+<?php endif; ?>
 	<h1>Kursus Alkitab Kita</h1>
 	<div id="tutor-index-main-info-blurb-container">
 		<p>Kami menyediakan kursus alkitabiah yang komprehensif untuk melengkapi secara rohani dan intelektual setiap orang percaya agar dapat menyelesaikan Amanat Agung.</p>

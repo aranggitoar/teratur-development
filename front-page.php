b4b9de0c2ecd632b44b6c88b88633fbf1a5052e5
aranggitoar <aranggi.josef@gmail.com>
2021-10-05 10:52:16 +0700

    Style: Revise the arrow symbol of the front page

--- front-page.php
+++ front-page.php
@@ -18,16 +18,16 @@ get_template_part( 'wp-load.php' );
 		</section>
 		<section>
 			<div>
-				<a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'TSI', OBJECT, 'bible-reader' ) ) ); ?>"><p>Alkitab dalam Terjemahan Sederhana</p><p>»</p></a>
+				<a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'TSI', OBJECT, 'bible-reader' ) ) ); ?>"><p>Alkitab dalam Terjemahan Sederhana</p><p>‣</p></a>
 			</div>
 			<div>
-				<a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'Petunjuk Penggunaan Bibledit' ) ) ); ?>"><p>Alat Penerjemahan Alkitab</p><p>»</p></a>
+				<a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'Petunjuk Penggunaan Bibledit' ) ) ); ?>"><p>Alat Penerjemahan Alkitab</p><p>‣</p></a>
 			</div>
 			<div>
-				<a href="<?php echo esc_attr( get_post_type_archive_link( 'courses' ) ); ?>"><p>Kursus Alkitab Kita</p><p>»</p></a>
+				<a href="<?php echo esc_attr( get_post_type_archive_link( 'courses' ) ); ?>"><p>Kursus Alkitab Kita</p><p>‣</p></a>
 			</div>
 			<div>
-				<a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'Artikel' ) ) ); ?>"><p>Artikel</p><p>»</p></a>
+				<a href="<?php echo esc_attr( get_permalink( get_page_by_title( 'Artikel' ) ) ); ?>"><p>Artikel</p><p>‣</p></a>
 			</div>
 		</section>
 	</div>

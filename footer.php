991c675dfa5c1a923f48b7e56817fb3f40454a59
aranggitoar <aranggi.josef@gmail.com>
2021-10-05 10:51:29 +0700

    Style: Add markup and style for footer

--- footer.php
+++ footer.php
@@ -13,21 +13,48 @@ if ( ! is_front_page() ) {
 	?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'teratur' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'teratur' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'teratur' ), 'teratur', '<a href="https://aranggitoar.com/">Aranggi Toar</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<hr>
+		<div class="container">
+			<div class="row">
+				<div>
+					<h5>Tentang Kami</h5>
+					<p>Alkitab Kita adalah suatu pelayanan daring interdenominasional yang didukung dan diluncurkan oleh Yayasan Alkitab Bahasa Kita (Albata). Alkitab Kita hadir untuk memperlengkapi setiap anggota Tubuh Kristus memahami, menafsirkan dan mengajar Firman Tuhan di mana saja, kapan saja, bagaikan penerjemah Alkitab.</p>
+					<p>Kami berharap untuk melakukan itu lewat menyediakan: (1) materi berupa kursus dan artikel, (2) pendidikan Kristen yang alkitabiah, berkualitas, dan gratis, serta (3) akses kepada alat-alat penyelidikan Alkitab.</p> 
+				</div>
 
+				<div>
+					<h5>Tautan-tautan Penting</h5>
+					<ul class="footer-links">
+						<li><a href="https://alkitabkita.info">Beranda</a></li>
+						<li><a href="https://alkitabkita.info/tentang-kami">Tentang Kami</a></li>
+						<li><a href="https://alkitabkita.info/alkitab/tsi">Baca TSI</a></li>
+						<li><a href="https://alkitabkita.info/petunjuk-penggunaan-bibledit">Petunjuk Penggunaan Bibledit</a></li>
+						<li><a href="https://alkitabkita.info/kursus">Kursus Alkitab Kita</a></li>
+						<li><a href="https://alkitabkita.info/artikel">Artikel</a></li>
+						<li><a href="https://alkitabkita.info/peta-situs">Peta Situs</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div>
+					<p class="copyright-text">Hak Cipta &copy; 2021
+						<a href="https://albata.info">Yayasan Alkitab BahasaKita</a>
+					</p>
+				</div>
+
+				<div>
+					<ul class="social-icons">
+						<li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+						<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+					</ul>
+				</div>
+			</div>
+		</div>
+</footer>
 	<?php
 };
 wp_footer();
